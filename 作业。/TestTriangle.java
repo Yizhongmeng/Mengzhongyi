16188b96559ed04232b4d15116098e5adce92914
Yizhongmeng <591220496@qq.com>
2016-05-17 17:57:18 +0800

    Update TestTriangle.java

--- 作业。/TestTriangle.java
+++ 作业。/TestTriangle.java
@@ -1,266 +1,55 @@
-package Mzy;
+package WSSKY;
+
 import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
+public class M44 {
 
-public class TestTriangle {
+	private int A;
+	private int B;
+	private int C;
+
+	public M44(int AA, int BB, int CC) {
+		this.A = AA;
+		this.B = BB;
+		this.C = CC;
+	}
+
+	@Parameters
+	public static Collection addedNumbers() {
+		return Arrays.asList(new Integer[][] { {4,5,6}, {4,4,5 },
+				                               {4,5,5 }, {4,5,4 },
+				                               {4,4,4},{3,4,5} });
+	}
+
 	@Test
-    public void testIsTriangle1(){
-        Triangle t = new Triangle(4,5,6);
+    public void testIsTriangle(){
+        M00 t = new M00( A,B,C);
         assertTrue(t.isTriangle(t));
-       
     }
-
-    @Test
+	@Test
     public void testIsTriangle2(){
-        Triangle t = new Triangle(4,4,5);
-        assertEquals("Isoceles",t.getType(t));
-    }
-
-    @Test
-    public void testIsTriangle3(){
-        Triangle t = new Triangle(4,5,5);
-        assertEquals("Isoceles",t.getType(t));
-    }
-   
-    @Test
-    public void testIsTriangle4(){
-        Triangle t = new Triangle(4,5,4);
-        assertEquals("Isoceles",t.getType(t));
-    }
-    @Test
-    public void testIsTriangle5(){
-        Triangle t = new Triangle(4,4,4);
+        M00 t = new M00(A,B,C);
         assertEquals("Regular",t.getType(t));
     }
-    @Test
-    public void testIsTriangle6(){
-        Triangle t = new Triangle(3,4,5);
-        assertEquals("Right",t.getType(t));
+	@Test
+    public void testIsTriangle3(){
+       M00 t = new M00(A,B,C);
+     assertEquals("Right",t.getType(t));
+     }
+	@Test
+    public void testIsTriangle4(){
+        M00 t = new M00(4,5,4);
+        assertEquals("Isoceles",t.getType(t));
     }
-    @Test
-    public void testIsTriangle7(){
-        Triangle t = new Triangle(4,5.1,6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle8(){
-        Triangle t = new Triangle(4,5,6.1);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle9(){
-        Triangle t = new Triangle(4.1,5.1,6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle10(){
-        Triangle t = new Triangle(4.1,5,6.1);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle11(){
-        Triangle t = new Triangle(4.1,5.1,6.1);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle12(){
-        Triangle t = new Triangle(4, , );
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle13(){
-        Triangle t = new Triangle( ,5, );
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle14(){
-        Triangle t = new Triangle(4,5,);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle15(){
-        Triangle t = new Triangle(4,,6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle16(){
-        Triangle t = new Triangle(, 5,6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle17(){
-        Triangle t = new Triangle( , 5,6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle18(){
-        Triangle t = new Triangle(-4,5,6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle19(){
-        Triangle t = new Triangle(4,-5,6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle20(){
-        Triangle t = new Triangle(4,5,-6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle21(){
-        Triangle t = new Triangle(-4,-5,6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle22(){
-        Triangle t = new Triangle(-4,5,-6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle23(){
-        Triangle t = new Triangle(4,-5,-6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle24(){
-        Triangle t = new Triangle(-4,-5,-6);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle25(){
-        Triangle t = new Triangle(1,5,7);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle26(){
-        Triangle t = new Triangle(2,5,7);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle27(){
-        Triangle t = new Triangle(4,1,7);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle28(){
-        Triangle t = new Triangle(4,3,7);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle29(){
-        Triangle t = new Triangle(7,5,1);
-        assertTrue(t.isTriangle(t));
-    }
-    @Test
-    public void testIsTriangle30(){
-        Triangle t = new Triangle(7,5,2);
-        assertTrue(t.isTriangle(t));
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
+	
+	
 }
-
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-
